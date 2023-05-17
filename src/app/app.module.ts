9a292593a107090d12fd1b84ef217d90008f098a
laska-adief <laskaadief.la@gmail.com>
2023-05-17 11:46:53 +0700

    [Company Profile Web] App root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { WhoWeAreComponent } from './pages/who-we-are/who-we-are.component';
+import { OurCoreValueComponent } from './pages/our-core-value/our-core-value.component';
+import { OurSpecialityComponent } from './pages/our-speciality/our-speciality.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroComponent,
+    WhoWeAreComponent,
+    OurCoreValueComponent,
+    OurSpecialityComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
